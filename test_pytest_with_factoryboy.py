c7dbd0054233b39b9da1badb52983914562b22c2
dimazest <dimazest@gmail.com>
2020-07-21 10:33:57 -0700

    Somehow working solution.

--- test_pytest_with_factoryboy.py
+++ test_pytest_with_factoryboy.py
@@ -1,15 +1,13 @@
 import factory
 
 import pytest
-import pytest_factoryboy
+from pytest_factoryboy import register, LazyFixture
 
 
 @pytest.fixture
 def spark_session():
     class SparkSession:
         def createDataframe(self, data, schema):
-            assert data is not None
-
             return {"spark_session": self, "data": data, "schema": schema}
 
     return SparkSession()
@@ -23,7 +21,7 @@ class DataFrameFactory(factory.Factory):
     class Meta:
         model = DataFrame
 
-    spark_session = pytest_factoryboy.LazyFixture("spark_session")
+    spark_session = LazyFixture("spark_session")
     data = None
     schema = None
 
@@ -33,17 +31,12 @@ class DataFrameFactory(factory.Factory):
         return spark_session.createDataframe(data, schema)
 
 
-pytest_factoryboy.register(DataFrameFactory)
-
-
-def test_data_frame(data_frame, spark_session):
-    assert data_frame["spark_session"] is spark_session
-
-
 class FunctionCall:
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
+
+        self.other_df = {}
 
         for k, v in kwargs.items():
             setattr(self, k, v)
@@ -51,28 +44,34 @@ class FunctionCall:
     def __call__(self):
         return {"df": self.df, "other_df": self.other_df}
 
-    def _to_expected(self):
+    def _as_expected(self):
         result = self()
 
-        return result["df"]["data"], result["other_df"]["data"]
+        return result["df"]["data"]
 
 
 class FunctionCallFactory(factory.Factory):
     class Meta:
         model = FunctionCall
 
-    df = factory.SubFactory(DataFrameFactory)
-    other_df = factory.SubFactory(DataFrameFactory)
+    spark_session = LazyFixture("spark_session")
+
+    df_data = None
+    df_schema = None
+    df = factory.LazyAttribute(lambda o: o.spark_session.createDataframe(o.df_data, o.df_schema))
+
+    other_df_data = None
+    other_df = factory.LazyAttribute(lambda o: o.spark_session.createDataframe(o.other_df_data, None))
 
 
-pytest_factoryboy.register(FunctionCallFactory, "fc")
+register(FunctionCallFactory, "fc")
 
 
-@pytest.mark.parametrize("fc__df__data,fc__other_df__data", [
+@pytest.mark.parametrize("fc__df_data,other_df__data", [
     ([1, 1], ["a", "b"]),
     (1, "a"),
 ])
-def test_function_call(fc, spark_session, fc__df__data, fc__other_df__data):
+def test_function_call(fc, spark_session, fc__df_data, other_df__data):
     assert fc.df["spark_session"] is spark_session
 
-    assert fc._as_expected() == (fc__df__data, fc__other_df__data)
+    assert fc._as_expected() == fc__df_data
