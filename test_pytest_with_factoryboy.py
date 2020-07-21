0cb924739ceb67076caf7dd105a4422ca311e39f
dimazest <dimazest@gmail.com>
2020-07-21 14:10:54 -0700

    Prototype

--- test_pytest_with_factoryboy.py
+++ test_pytest_with_factoryboy.py
@@ -31,12 +31,53 @@ class DataFrameFactory(factory.Factory):
         return spark_session.createDataframe(data, schema)
 
 
-class FunctionCall:
+import inspect
+import dataclasses
+
+from factory.base import FactoryMetaClass
+
+
+class BaseFabric:
+    pass
+
+
+class DataFrame(BaseFabric):
+
+    def __init__(self, data="some_data", schema="some_schema"):
+        self.data = data
+        self.schema = schema
+
+    def attrs(self, name):
+        return {
+            f"{name}_data": self.data,
+            f"{name}_schema": self.schema,
+            name: factory.LazyAttribute(lambda o: o.spark_session.createDataframe(
+                data=getattr(o, f"{name}_data"),
+                schema=getattr(o, f"{name}_schema")
+            )),
+        }
+
+
+class DFFactoryMetaClass(FactoryMetaClass):
+
+    def __new__(mcs, class_name, bases, attrs):
+
+        for attr, annotation in attrs.get("__annotations__", {}).items():
+            if isinstance(annotation, BaseFabric):
+                attrs.update(annotation.attrs(attr))
+
+        return super().__new__(mcs, class_name, bases, attrs)
+
+
+class DFFactory(factory.Factory, metaclass=DFFactoryMetaClass):
+    pass
+
+
+
+class FunctionCall():
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
-
-        self.other_df = {}
 
         for k, v in kwargs.items():
             setattr(self, k, v)
@@ -49,19 +90,22 @@ class FunctionCall:
 
         return result["df"]["data"]
 
+    def create(self, *args, **kwargs):
+        return super()._create(args, kwargs)
 
-class FunctionCallFactory(factory.Factory):
+
+class FunctionCallFactory(DFFactory):
     class Meta:
         model = FunctionCall
 
     spark_session = LazyFixture("spark_session")
 
-    df_data = None
-    df_schema = None
-    df = factory.LazyAttribute(lambda o: o.spark_session.createDataframe(o.df_data, o.df_schema))
+    df: DataFrame()
+    other_df: DataFrame(schema=["id", "key", "value"])
 
-    other_df_data = None
-    other_df = factory.LazyAttribute(lambda o: o.spark_session.createDataframe(o.other_df_data, None))
+    # df_data = None
+    # df_schema = None
+    # df = factory.LazyAttribute(lambda o: o.spark_session.createDataframe(o.df_data, o.df_schema))
 
 
 register(FunctionCallFactory, "fc")
@@ -75,3 +119,4 @@ def test_function_call(fc, spark_session, fc__df_data, other_df__data):
     assert fc.df["spark_session"] is spark_session
 
     assert fc._as_expected() == fc__df_data
+    assert fc.other_df_schema == ["id", "key", "value"]
